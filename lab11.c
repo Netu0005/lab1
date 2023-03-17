9d998ffae94c6ed0615fc42309738ba7cfb362dd
Netu0005 <124419883+Netu0005@users.noreply.github.com>
2023-03-17 16:58:57 +0700

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-void init(int* arr, int n)
+void init(int** arr, int n)
 {
-        arr = malloc(n * sizeof(int));
+        *arr = malloc(n * sizeof(int));
         int i;
         for (i = 0; i<n; ++i)
         {
-                arr[i] = i;
+                *(*arr+i) = i;
         }
 }
 int main()
 {
         int* arr = NULL;
         int n = 10;
-        init(arr, n);
+        init(&arr, n);
         int i;
         for(i = 0; i < n; ++i)
         {
